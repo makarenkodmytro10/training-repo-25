56475df9b8588421b49bcd22d83a5e311585dbeb
makarenkodmytro10 <zelenskiy78ukraine@gmail.com>
2025-10-02 18:59:52 +0300

    Fix apiUrl error in makarenkodmytro10/GitHubUserStatistics.java

--- makarenkodmytro10/GitHubUserStatistics.java
+++ makarenkodmytro10/GitHubUserStatistics.java
@@ -7,7 +7,7 @@ public class GitHubUserStatistics {
     public final static String USER_NAME;
 
     public static void main(String[] args) {
-        String apiUrl = "https://api.github.com/users/";
+        String apiUrl = "https://api.github.com/users/" + USER_NAME;
         try {
             @SuppressWarnings("deprecation")
             URL url = new URL(apiUrl);
