6f09d6e0c3a352a3e9922bc9820cbd43989f9ce5
makarenkodmytro10 <zelenskiy78ukraine@gmail.com>
2025-10-02 18:51:16 +0300

    Fix USER_NAME error in makarenkodmytro10/GitHubUserStatistics.java

--- makarenkodmytro10/GitHubUserStatistics.java
+++ makarenkodmytro10/GitHubUserStatistics.java
@@ -4,7 +4,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class GitHubUserStatistics {
-    public final static String USER_NAME;
+    public final static String USER_NAME = "makarenkodmytro10";
 
     public static void main(String[] args) {
         String apiUrl = "https://api.github.com/users/";
